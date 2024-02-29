ca6c46f4dd8c4d1f1c7669e47478121d451869e3
puviththira <puvipuviththira111@gmail.com>
2024-02-29 13:47:00 +0530

    category part uploarded

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ import colors from "colors";
 import dotenv from "dotenv";
 import connectDB from "./config/mongoose.js";
 import cors from "cors";
+import authRoutes from "./routes/authRoute.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
+
 
 dotenv.config();
 connectDB();
@@ -11,7 +14,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/category", categoryRoutes);
 
 
 app.get("/", (req, res) => {
