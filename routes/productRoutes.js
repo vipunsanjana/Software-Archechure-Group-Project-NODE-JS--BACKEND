224802607a46f78f92fd41c2810ac7025f431d96
Vipun Sanjana Jayathunga <92296561+vipunsanjana@users.noreply.github.com>
2024-03-03 14:35:47 +0530

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -21,7 +21,6 @@ import formidable from "express-formidable";
 
 const router = express.Router();
 
-//routes
 router.post(
   "/create-product",
   requireSignIn,
@@ -29,7 +28,7 @@ router.post(
   formidable(),
   createProductController
 );
-//routes
+
 router.put(
   "/update-product/:pid",
   requireSignIn,
@@ -38,28 +37,25 @@ router.put(
   updateProductController
 );
 
-//get products
+
 router.get("/get-product", getProductController);
 
-//single product
 router.get("/get-product/:slug", getSingleProductController);
 
-//get photo
+
 router.get("/product-photo/:pid", productPhotoController);
 
-//delete rproduct
 router.delete("/delete-product/:pid", deleteProductController);
 
-//filter product
+
 router.post("/product-filters", productFiltersController);
 
-//product count
+
 router.get("/product-count", productCountController);
 
-//product per page
 router.get("/product-list/:page", productListController);
 
-//search product
+
 router.get("/search/:keyword", searchProductController);
 
 router.get("/related-product/:pid/:cid", realtedProductController);
@@ -69,7 +65,7 @@ router.get("/product-category/:slug", productCategoryController);
 
 
 router.get("/braintree/token", braintreeTokenController);
-// //payments
+
 router.post("/braintree/payment", requireSignIn, brainTreePaymentController);
 
 export default router;
